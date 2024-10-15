6b81aa27d0cb99256b792a32087d5e5cd6f6c6df
Saulo <saulobf8@gmail.com>
2024-10-15 16:53:32 -0300

    Criando Crude de Professor e Aluno

--- aluno_online/api/src/main/java/br/com/alunoonline/api/controller/ProfessorController.java
+++ aluno_online/api/src/main/java/br/com/alunoonline/api/controller/ProfessorController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/professor")
 public class ProfessorController {
@@ -17,5 +20,29 @@ public class ProfessorController {
     public void criarProfessor (@RequestBody Professor professor) {
 
         professorService.criarProfessor(professor);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Professor> listarTodosProfessor() {
+        return professorService.listarTodosProfessor();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<Professor> buscarProfessorPorId(@PathVariable Long id) {
+        return professorService.buscarProfessorPorId(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletarProfessorPorId(@PathVariable Long id) {
+        professorService.deleterProfessorPorId(id);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void AtualizarProfessorPorId(@PathVariable Long id, @RequestBody Professor professor){
+        professorService.atualizarProfessorPorId(id, professor);
     }
 }
